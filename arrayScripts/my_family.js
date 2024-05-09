535a466e2c1a382980dcf173152759350272928d
IsaacRodriguezDev <isaacrodriguez.dev@gmail.com>
2024-05-09 11:36:21 -0500

    completed my_family.js and for_loops.js

---
+++ arrayScripts/my_family.js
@@ -0,0 +1,4 @@
+let names = ['Charles','Cassy','James','Alexs']
+for(let i = 0; i <names.length;i++){
+    console.log(names[i])
+}
---
+++ loops_scripts/for_loops.js
@@ -0,0 +1,8 @@
+
+
+
+for (let i = 0; i < 7; i++){
+    let lovingLoops = 'I love Loops'
+    
+    console.log(lovingLoops)
+}
