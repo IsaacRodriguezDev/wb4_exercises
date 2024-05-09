5436b0864ecd7f32a163ec640ab3bc9a762fc64f
IsaacRodriguezDev <isaacrodriguez.dev@gmail.com>
2024-05-09 11:35:25 -0500

    completed while_loops.js

---
+++ loops_scripts/while_loops.js
@@ -0,0 +1,8 @@
+// created a variable named num and set it equal to 0
+let num = 0
+
+// created a while loop with the condition if variable num is less then 7 console.log and add 1 to the variable until false
+while(num < 7){
+    console.log('I love loops')
+    num++
+}
